591eb4f26448e7a3ea9883c82811cb8ae8e6784a
beatface <emma.beaton@gmail.com>
2015-12-07 15:49:06 -0600

    got modal working with api data

--- app/controllers/main-page.js
+++ app/controllers/main-page.js
@@ -2,14 +2,18 @@
 app.controller("mainPageCtrl", ["$http", "$scope", "$firebaseArray", function($http, $scope, $firebaseArray) {
 
 	$scope.submitSearch = function() {
+		console.log("you clicked submit search");
+	};
 
+	$scope.uploadPin = function() {
 		$scope.urlToSearch = $scope.url;
+		console.log("url", $scope.url);
 
 		$http.get("http://api.embed.ly/1/extract?key=514b5e76363e48c7892110e2bd33a491&url=" + $scope.urlToSearch + "&maxwidth=500")
 		.then(function(data) {
 			console.log("data", data);
 		});
-	}
+	};
 
 
 }]);
